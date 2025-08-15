0763020eca579a940696d44128f3604c07b96388
Gabriel Marinho <git@gabrielmarinho.org>
2025-08-15 20:50:10 -0300

    adds few new plugins on Vim to improve development experience

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,17 @@
 call plug#begin()
 
-Plug 'jiangmiao/auto-pairs'
+	Plug 'jiangmiao/auto-pairs'
+	Plug 'preservim/nerdtree'
+	Plug 'ryanoasis/vim-devicons'
+	Plug 'junegunn/fzf'
+	Plug 'nvimdev/dashboard-nvim'
 
 call plug#end()
 
+autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in")
+  \ | execute 'NERDTree'
+  \ | wincmd p
+  \ | execute 'Dashboard'
+  \ | endif
+
+set encoding=UTF-8
