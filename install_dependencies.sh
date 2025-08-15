cce4ca06d5f8a8520ff3d1609b36157561ac9735
Gabriel Marinho <git@gabrielmarinho.org>
2025-08-15 14:13:15 -0300

    adds shell to install basic dependencies to configure env

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install zsh git curl nvim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
