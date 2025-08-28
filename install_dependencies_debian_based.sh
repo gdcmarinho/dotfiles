5af6864c9ffb392545bef55366f91cc840cd444c
Gabriel Marinho <git@gabrielmarinho.org>
2025-08-28 13:28:13 -0300

    adds commands to install ohmyzsh plugins

--- install_dependencies_debian_based.sh
+++ install_dependencies_debian_based.sh
@@ -4,3 +4,8 @@ sudo apt-get upgrade
 sudo apt-get install zsh git curl nvim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+source .zshrc
